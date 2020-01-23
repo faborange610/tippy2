a90b6ac455660c57c7d40246ad7142f3936e979e
Songeun Kim <faborange610@gmail.com>
2020-01-22 19:14:05 -0800

    Initial Commit

---
+++ tippy2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy2
+//
+//  Created by 김송은 on 1/22/20.
+//  Copyright © 2020 김송은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
